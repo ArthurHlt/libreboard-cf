b2938c4ab57904d237de28cfac2ea6917d73bacd
yasar icli <yasaricli@gmail.com>
2014-01-22 16:46:53 +0100

    login and register

--- client/apps/accounts/accounts.js
+++ client/apps/accounts/accounts.js
@@ -9,17 +9,66 @@
 
 if (Meteor.isClient) {
 
-    Template.login.helpers({});
+    // Login Helpers
+    Helpers("login", {});
+
+    // login rendered
     Rendered("login", function(addClass) {
         addClass("account-page");
     });
 
+    // login events
+    Template.login.events({
+        'submit #LoginForm' : function(e, t){
+            e.preventDefault();
+            var email = trimInput(t.find('#email-login').value.toLowerCase()),
+                password = t.find('#password-login').value;
 
-    Template.signup.helpers({});
+            if (isNotEmpty(email) && isNotEmpty(password)) {
+                Meteor.loginWithPassword(email, password, function(err ){
+                    if (err) {
+                        error("The email or password was incorrect."); 
+                        return;    
+                    }
+
+                    // login then 
+                });
+            }
+        }
+    });
+
+
+    // Signup helpers
+    Helpers("signup", {});
+
+
+    // Rendered signup
     Rendered("signup", function(addClass) {
         addClass("account-page");
     });
 
+
+    // signup events
+    Template.signup.events({
+        'submit #SignUpForm' : function(e, t) {
+            e.preventDefault();
+            var fullname = trimInput(t.find('#display-name-create').value.toLowerCase()),
+                email = trimInput(t.find('#email-create').value.toLowerCase()),
+                password = t.find('#password-create').value;
+
+            if (isNotEmpty(email) && isNotEmpty(email) && 
+                isNotEmpty(password) && isEmail(email) && 
+                isValidPassword(password)) {
+
+                removeError(); 
+                Accounts.createUser({name: fullname, email: email, password : password}, function(err) { 
+              
+                   // redirect 
+                });
+            }
+             
+        }
+    });
 
     Template.forgot.helpers({});
     Rendered("forgot", function(addClass) {
