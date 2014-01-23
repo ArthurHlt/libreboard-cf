cf6ede9c3f8e3f205ea5add678e1b5f35ec5e93c
Yasar icli <yasaricli@gmail.com>
2014-01-23 13:19:49 +0000

    update query fix

--- collections/list.js
+++ collections/list.js
@@ -6,7 +6,6 @@ if (Meteor.isClient) {
     ListQuery = CollectionQuery.extend({
         addList: function(title, board_id) {
             this.collect(Lists).only(function(insert, update, remove) {
-           
                 // insert 
                 insert({ title: title, board_id: board_id });
 
@@ -16,7 +15,6 @@ if (Meteor.isClient) {
         },
         updateListTitle: function(list_id, title) {
             this.collect(Lists).only(function(insert, update) {
-           
                 update(list_id, { title: title });  
             });
         }
