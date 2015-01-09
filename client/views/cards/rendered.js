ArthurHlt/libreboard-cf

commit 5fb35d15becc13f0cc4a1dde167bae0a4fa75d02

Remove unnecessary `updateCard` method, use minimongo instead

client/views/cards/rendered.js
@@ -16,11 +16,12 @@ Template.cards.rendered = function() {
             stop: function(event, ui) {
                 var data = new Utils.getCardData(ui.item);
 
-                // update card
-                Meteor.call('updateCard', data.cardId, {
-                    oldListId: data.oldListId,
-                    listId: data.listId,
-                    sort: data.sort
+                Cards.update(data.cardId, {
+                    $set: {
+                        oldListId: data.oldListId,
+                        listId: data.listId,
+                        sort: data.sort
+                    }
                 });
             }
         }).disableSelection();

client/views/cards/events.js
@@ -104,10 +104,11 @@ Template.cardDetailWindow.events({
     'submit #WindowTitleEdit': function(event, t) {
         var title = t.find('#title').value;
         if ($.trim(title)) {
-            Meteor.call('updateCard', this.card._id, {
-                title: title
-            }, function(err) {
-                // close editing
+            Cards.update(this.card._id, {
+                $set: {
+                    title: title
+                }
+            }, function (err, res) {
                 if (!err) $('.editing').removeClass('editing');
             });
         }

server/methods.js
@@ -19,12 +19,5 @@ Meteor.methods({
                 approved: false
             }
         });
-    },
-    updateCard: function(_id, $set) {
-        check(_id, String);
-        check($set, Object);
-        Cards.update(_id, {
-            $set: $set
-        });
     }
 });

commit 5ff6e87257e25864cd10f2b5767c6070f514db5f

Hacky fix for the drag and drop between lists random bug
The bug was introduced in 5fb35d1.
Closes #61.

client/views/cards/rendered.js
@@ -16,7 +16,16 @@ Template.cards.rendered = function() {
             stop: function(event, ui) {
                 var data = new Utils.getCardData(ui.item);
 
-                Cards.update(data.cardId, {
+                // XXX For some reason, running the latency compensation method
+                // on the client randomly raise a bug (#61) in which the drag
+                // and drop operation is visually reversed after a moment -- but
+                // the document is correctly updated and a page refresh shows it
+                // correctly. It seems to happen when the server overwrite the
+                // latency compensated result in the DOM.
+                // The hack used here is to skirt the client operation by
+                // directly calling the server underlined method using the
+                // private `_collection` field.
+                Cards._collection.update(data.cardId, {
                     $set: {
                         listId: data.listId,
                         sort: data.sort