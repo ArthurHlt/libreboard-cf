ArthurHlt/libreboard-cf

commit 6c73ac317d6a7c5efb2c82975ca3e6a2f049e3a9

Avoid a race condition in the `board` composite publication
This race condition caused bug #61, and previous hacky fix to it are
not needed anymore.

Fixes #61
Closes #78

client/views/cards/rendered.js
@@ -15,16 +15,7 @@ Template.cards.rendered = function() {
             },
             stop: function(event, ui) {
                 var data = new Utils.getCardData(ui.item);
-
-                // XXX For some reason, running the latency compensation method
-                // on the client randomly raise a bug (#61) in which the drag
-                // and drop operation is visually reversed after a moment -- but
-                // the document is correctly updated and a page refresh shows it
-                // correctly. It seems to happen when the server overwrite the
-                // latency compensated result in the DOM.
-                // The hack used here is to skirt the client operation by
-                // directly calling the server underlined method.
-                Meteor.call('/cards/update', {_id: data.cardId}, {
+                Meteor.update(data.cardId, {
                     $set: {
                         listId: data.listId,
                         sort: data.sort

server/publications/boards.js
@@ -47,31 +47,53 @@ Meteor.publishComposite('board', function(boardId, slug) {
         // in this publication, and instead use the card publication for that
         // purpose.
         children: [
-            // Lists, cards, and cards comments
+            // Lists
             {
                 find: function(board) {
                     return Lists.find({
                         boardId: board._id
                     });
-                },
-                children: [
-                    {
-                        find: function(list, board) {
-                            return Cards.find({
-                                listId: list._id
-                            });
-                        },
+                }
+            },
 
-                        children: [
-                            // Card comments
-                            {
-                                find: function(card) {
-                                    return CardComments.find({
-                                        cardId: card._id
-                                    });
-                                }
-                            }
-                        ]
+            // Cards and cards comments
+            // XXX Originally we were publishing the card documents as a child
+            // of the list publication defined above using the following
+            // selector `{ listId: list._id }`. But it was causing a race
+            // condition in publish-composite, that I documented here:
+            //
+            //   https://github.com/englue/meteor-publish-composite/issues/29
+            //
+            // I then tried to replace publish-composite by cottz:publish, but
+            // it had a similar problem:
+            //
+            //   https://github.com/Goluis/cottz-publish/issues/4
+            //   https://github.com/libreboard/libreboard/pull/78
+            //
+            // The current state of relational publishing in meteor is a bit
+            // sad, there are a lot of various packages, with various APIs, some
+            // of them are unmaintained. Fortunately this is something that will
+            // be fixed by meteor-core at some point:
+            //
+            //   https://trello.com/c/BGvIwkEa/48-easy-joins-in-subscriptions
+            //
+            // And in the meantime our code below works pretty well -- it's not
+            // even a hack!
+            {
+                find: function(board) {
+                    return Cards.find({
+                        boardId: board._id
+                    });
+                },
+
+                children: [
+                    // Card comments
+                    {
+                        find: function(card) {
+                            return CardComments.find({
+                                cardId: card._id
+                            });
+                        }
                     }
                 ]
             },

commit c1544e405694960d5e9f0b1f00b81714677356bd

Fix #79
Bug introduced in 6c73ac317d

client/views/cards/rendered.js
@@ -15,7 +15,7 @@ Template.cards.rendered = function() {
             },
             stop: function(event, ui) {
                 var data = new Utils.getCardData(ui.item);
-                Meteor.update(data.cardId, {
+                Cards.update(data.cardId, {
                     $set: {
                         listId: data.listId,
                         sort: data.sort